6f829128c8efdf61c47e17ab327be53d7d70c00d
Mateus <mateuspastori@outlook.com>
2019-01-16 23:31:19 -0200

    sdgdhg

--- meat-app-starter/src/app/shared/radio/radio.component.ts
+++ meat-app-starter/src/app/shared/radio/radio.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
-
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  
   @Input() options: RadioOption[];
 
   value: any;
+  onChange: any;
   constructor() { }
 
   ngOnInit() {
@@ -17,5 +26,20 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
   }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
+  }
+
 }
