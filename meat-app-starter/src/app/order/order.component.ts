6bee99a68833f50e231500cee111e9d5c4bad1d8
Mateus <mateuspastori@outlook.com>
2019-01-22 13:05:54 -0200

    order items

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { RadioOption } from '../shared/radio/radio-option';
+import {Component, OnInit} from '@angular/core';
+import {RadioOption} from '../shared/radio/radio-option';
+import {OrderService} from "./order.service";
+import {CartItem} from "../restaurant-detail/shopping-cart/cart-item.model";
 
 @Component({
-  selector: 'mt-order',
-  templateUrl: './order.component.html'
+    selector: 'mt-order',
+    templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
 
-  paymentOptions: RadioOption[] = [
-    {label: 'Dinheiro', value: 'MON'},
-    {label: 'Cartão Débito', value: 'DEB'},
-    {label: 'Cartão Refeição', value: 'REF'}
-  ];
+    paymentOptions: RadioOption[] = [
+        {label: 'Dinheiro', value: 'MON'},
+        {label: 'Cartão Débito', value: 'DEB'},
+        {label: 'Cartão Refeição', value: 'REF'}
+    ];
 
-  constructor() { }
+    constructor(private orderService: OrderService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    cartItems(): CartItem[] {
+        return this.orderService.cartItems();
+    }
+
+    increaseQty(item: CartItem) {
+        this.orderService.increaseQty(item);
+    }
+
+    decreaseQty(item: CartItem) {
+        this.orderService.decreaseQty(item);
+    }
+
+    remove(item: CartItem) {
+        this.orderService.remove(item);
+    }
 }
--- meat-app-starter/src/app/shared/radio/radio.component.ts
+++ meat-app-starter/src/app/shared/radio/radio.component.ts
@@ -1,45 +1,50 @@
-import { Component, OnInit, Input, forwardRef } from '@angular/core';
-import { RadioOption } from './radio-option';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {Component, OnInit, Input, forwardRef} from '@angular/core';
+import {RadioOption} from './radio-option';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
-  selector: 'mt-radio',
-  templateUrl: './radio.component.html',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => RadioComponent),
-      multi: true
-    }
-  ]
+    selector: 'mt-radio',
+    templateUrl: './radio.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => RadioComponent),
+            multi: true
+        }
+    ]
 })
 export class RadioComponent implements OnInit, ControlValueAccessor {
-  
-  @Input() options: RadioOption[];
 
-  value: any;
-  onChange: any;
-  constructor() { }
+    @Input() options: RadioOption[];
 
-  ngOnInit() {
-  }
+    value: any;
+    onChange: any;
 
-  setValue(value: any) {
-    this.value = value;
-    this.onChange(this.value);
-  }
+    constructor() {
+    }
 
-  writeValue(obj: any): void {
-    this.value = obj;
-  }
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-  registerOnTouched(fn: any): void {
-    throw new Error("Method not implemented.");
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    throw new Error("Method not implemented.");
-  }
+    ngOnInit() {
+    }
+
+    setValue(value: any) {
+        this.value = value;
+        this.onChange(this.value);
+    }
+
+    writeValue(obj: any): void {
+        this.value = obj;
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        //throw new Error("Method not implemented.");
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        //throw new Error("Method not implemented.");
+    }
 
 }
