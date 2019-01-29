8970601e7892de92c8649a609ac0d6d5ccf75704
Mateus <mateuspastori@outlook.com>
2019-01-29 12:55:31 -0200

    rating

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -12,6 +12,7 @@ export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'order', component: OrderComponent},
+    {path: 'order-summary', component: OrderSummaryComponent},
     {path: 'restaurants', component: RestaurantsComponent},
     {
         path: 'restaurants/:id', component: RestaurantDetailComponent,
@@ -20,6 +21,5 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
         ]
-    },
-    {path: 'order-summary', component: OrderSummaryComponent}
+    }
 ];
---
+++ meat-app-starter/src/app/shared/rating/rating.component.ts
@@ -0,0 +1,44 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+    selector: 'mt-rating',
+    templateUrl: './rating.component.html'
+})
+export class RatingComponent implements OnInit {
+
+    @Output() rated = new EventEmitter<number>()
+
+    rates: number[] = [1, 2, 3, 4, 5];
+
+    rate: number = 0;
+
+    previousRate: number;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    setRate(r: number) {
+        this.rate = r;
+        this.previousRate = undefined;
+        this.rated.emit();
+    }
+
+    setTempRate(r: number) {
+        if(this.previousRate === undefined){
+            this.previousRate = this.rate;
+        }
+        this.rate = r;
+    }
+
+
+    clearTempRate() {
+        if(this.previousRate !== undefined){
+            this.rate = this.previousRate;
+            this.previousRate = undefined;
+        }
+    }
+}
+
