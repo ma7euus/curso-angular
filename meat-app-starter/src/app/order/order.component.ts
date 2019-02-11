e31d2f9bb48637469e17a820bd023d434531b040
Mateus <mateuspastori@outlook.com>
2019-02-10 23:31:30 -0200

    form controll

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import {OrderService} from "./order.service";
 import {CartItem} from "../restaurant-detail/shopping-cart/cart-item.model";
 import {OrderItemModel, OrderModel} from "./order.model";
 import {Router} from "@angular/router";
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'mt-order',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
 })
 export class OrderComponent implements OnInit {
 
+    orderForm: FormGroup;
     delivery: number = 8;
 
     paymentOptions: RadioOption[] = [
@@ -21,10 +23,20 @@ export class OrderComponent implements OnInit {
 
     constructor(
         private orderService: OrderService,
-        private router: Router) {
+        private router: Router,
+        private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
+        this.orderForm = this.formBuilder.group({
+            name: this.formBuilder.control(''),
+            email: this.formBuilder.control(''),
+            emailConfirmation: this.formBuilder.control(''),
+            address: this.formBuilder.control(''),
+            number: this.formBuilder.control(''),
+            optionalAddress: this.formBuilder.control(''),
+            paymentOption: this.formBuilder.control('')
+        });
     }
 
     itemsValue(): number {
