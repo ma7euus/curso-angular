0978f2d9602b0a2d220475445885a6a7a229e4e2
Mateus <mateuspastori@outlook.com>
2019-03-11 12:32:56 -0300

    server

--- meat-app-starter/backend/server.js
+++ meat-app-starter/backend/server.js
@@ -1,15 +1,12 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
-
-server.use(jsonServer.bodyParser)
-
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 // Use default router
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.use(router);
+server.listen(3000, function () {
+    console.log('JSON Server is running');
+});
